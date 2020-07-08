b77b280ed52c7c527c3e10c29d137f2c82a9a0e5
Michael Refvem <mkrefvem@gmail.com>
2020-07-08 14:46:42 -0700

    Challenges 1, 6, 7 complete

--- CSharpReviewTest/UnitTest1.cs
+++ CSharpReviewTest/UnitTest1.cs
@@ -1,14 +1,55 @@
 using System;
 using Xunit;
+using static CSharpReview.Program;
 
 namespace CSharpReviewTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(new int[] { 3, 3, 3 }, 27)]
+        public void ProdTest(int[] arr, int expected)
         {
+            Assert.Equal(expected, MultiplyInputNumber(arr));
+        }
 
+        [Fact]
+        public void CanReturnZero()
+        {
+            // Arrange
+            string input = "cat";
+
+            // Act
+            int outputFromMethod = MultiplyInputNumber(input);
+
+            // Assert
+            Assert.Equal(0, outputFromMethod);
+        }
+
+        [Fact]
+        public void CanReturnProductWithThreeNumbers()
+        {
+            // Arrange
+            string numbers = "2 3 4";
+
+            // Act
+            int output = MultiplyInputNumber(numbers);
+
+            // Assert
+            Assert.Equal(24, output);
+        }
+
+        [Fact]
+        public void InputMoreThanThreeNumbers()
+        {
+            // Arrange
+            string numbers = "3 4 5 6";
+
+            // Act
+            int output = MultiplyInputNumber(numbers);
+
+            // Assert
+            Assert.Equal(60, output);
         }
     }
 }
